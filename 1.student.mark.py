fa2adbad65fee1f52fed8f1abd7bccf20350bb0c
Kasumi <kiriha0205@gmail.com>
2024-01-15 18:30:50 +0700

    countine my lab 1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -30,22 +30,52 @@ def input_infor_course():
 def input_mark (course_id):
     for stu in student :
         mark_count = float(input("input mark for student {stu[1]} in course {course_id}"))
-        mark.append(stu , course_id , mark_count)
+        mark.append(course_id , stu , mark_count)
+
+
+def list_courses ():
+    for couses_1 in courses :
+        print (couses_1)
+        
+
+def list_students ():
+    for student_1 in student :
+        print(student_1)
+
+def show_students_with_cousrses (courses ):
+    for score in mark :
+        if score[0] == courses : 
+            print(f"student {mark[1]}  get score {mark[2]} in couses number {mark[0]} ")
+
+
+
 
 # main 
 def main (): 
     while True :
-        print (" 1 .  input information students ")
+        print (" 1 . input number student  ")
         print (" 2 .  input information couses ")
         print (" 3 .  input mark of couses ")
+        print ()
+        
+        
         print (" 0 : break ")
         
-        option = int(input("choose :x "))
+        option = int(input("choose : "))
         if option == 0 :
             print("ok , you break ")
             break 
         
         if option == 1 :
-            input_number_students
+            print(" input  number student : " )
+            number_student = input_number_students()
+            print(number_student)
+        
+
+
+
+
+
+
 if __name__ == "__main__":
     main()
